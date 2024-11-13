3a3e672835799a2214fbfb1dafbf9827563100fe
Ismail Çetin <isocetin1907@gmail.com>
2024-11-13 10:19:20 +0100

    Add DB_PORT configuration to application settings and update DB_HOST definition

--- config/application.php
+++ config/application.php
@@ -66,6 +66,7 @@ if (!env('WP_ENVIRONMENT_TYPE') && in_array(WP_ENV, ['production', 'staging', 'd
 Config::define('WP_HOME', env('WP_HOME'));
 Config::define('WP_SITEURL', env('WP_SITEURL'));
 Config::define('SITEURL', env('SITEURL'));
+Config::define('DB_PORT', env('DB_PORT'));
 
 /**
  * Custom Content Directory
@@ -84,7 +85,7 @@ if (env('DB_SSL')) {
 Config::define('DB_NAME', env('DB_NAME'));
 Config::define('DB_USER', env('DB_USER'));
 Config::define('DB_PASSWORD', env('DB_PASSWORD'));
-Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
+Config::define('DB_HOST', env('DB_HOST') . ':' . env('DB_PORT') ?: 'localhost');
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
